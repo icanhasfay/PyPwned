706387f8a71ad6b57ee370026b8257089c7c905e
Eric Fay <icanhasfay@gmail.com>
2016-09-23 17:15:10 -0700

    Real fix for Issue #3. Reconfigure setup.py to not reference pypwned package before setup. Bump to version 0.1.4

--- pypwned/__init__.py
+++ pypwned/__init__.py
@@ -1,5 +1,5 @@
 __author__ = 'Eric Fay'
-__version__ = "0.1.3"
+__version__ = "0.1.4"
 
 
 import requests
--- setup.py
+++ setup.py
@@ -1,19 +1,17 @@
-import pypwned
-
-from distutils.core import setup
+from setuptools import setup
 
 setup(
     name="pypwned",
     packages=["pypwned"],
     package_dir={"pypwned": "pypwned"},
-    version=pypwned.__version__,
+    version="0.1.4",
     description="A Python client for the HaveIBeenPwned REST API",
     long_description=open("README.md").read() + "\n\n" + open("CHANGES.rst").read(),
     author="Eric Fay",
     author_email="icanhasfay@gmail.com",
     url="https://github.com/icanhasfay/PyPwned",
     license=open("LICENSE.rst").read(),
-    install_requires=['requests'],
+    install_requires=["requests"],
     classifiers=(
         "Intended Audience :: Developers",
         "Intended Audience :: End Users/Desktop",
